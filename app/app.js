40a18c49a7eb2bd615580819d6def2f681d2d6c4
Willian Araujo <willian.araujo@sioux.ag>
2023-04-02 20:23:44 -0300

    Criando layout de tela desktop

--- app/app.js
+++ app/app.js
@@ -1,7 +1,22 @@
 const burgerMenu = document.querySelector('#burguer-menu');
 const menu = document.querySelector('#menu');
+const imageBurguerMenu = document.querySelector("#image-burguer");
+
+const imageCancelMenu = document.createElement('img');
+imageCancelMenu.src = "images/Vector.svg";
+imageCancelMenu.id = "imageCancelMenu";
+
+console.log(imageBurguerMenu, imageCancelMenu)
 
 
 burgerMenu.addEventListener('click', () => {
     menu.classList.toggle('openMenu');
+    
+    if (burgerMenu.contains(imageBurguerMenu)) {
+        burgerMenu.removeChild(imageBurguerMenu);
+        burgerMenu.appendChild(imageCancelMenu);
+    } else {
+        burgerMenu.removeChild(imageCancelMenu);
+        burgerMenu.appendChild(imageBurguerMenu);
+    }
 })
